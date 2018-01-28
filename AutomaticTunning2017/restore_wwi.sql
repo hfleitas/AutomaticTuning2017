8551064fb913418f96d7e412e10bc23b9acc2a68
Hiram Fleitas <hiramfleitas@hotmail.com>
2018-01-28 14:01:22 -0500

    added compat rollup and update stats

--- AutomaticTunning2017/restore_wwi.sql
+++ AutomaticTunning2017/restore_wwi.sql
@@ -1,8 +1,18 @@
-restore filelistonly from disk = 'c:\sql_sample_databases\wideworldimporters-full.bak'
+--verify
+restore filelistonly from disk = 'c:\users\hfleitas\downloads\wideworldimporters-full.bak'
+go 
+--set your own paths
+restore database [WideWordImporters] from disk = 'c:\users\hfleitas\downloads\wideworldimporters-full.bak'
+with move 'wwi_primary' to 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\DATA\wideworldimporters.mdf' 
+,move 'wwi_userdata' to 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\DATA\wideworldimporters_userdata.ndf'
+,move 'wwi_log' to 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\DATA\wideworldimporters.ldf'
+,move 'wwi_inmemory_data_1' to 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\DATA\wideworldimporters_inmemory_data_1'
+,stats = 25
 go
-restore database wideworldimporters from disk = 'c:\sql_sample_databases\wideworldimporters-full.bak'
-with move 'wwi_primary' to 'd:\data\wideworldimporters.mdf',
-move 'wwi_userdata' to 'd:\data\wideworldimporters_userdata.ndf',
-move 'wwi_log' to 'd:\data\wideworldimporters.ldf',
-move 'wwi_inmemory_data_1' to 'd:\data\wideworldimporters_inmemory_data_1'
+--rollup compat to 2017
+alter database [WideWordImporters] set compatibility_level = 140
 go
+--update stats
+use [WideWordImporters]
+go
+EXEC sp_updatestats 
